d837f669598cb7327340877b7b8c83ea5900f095
Nikko Rey <nikkoreyhular0818@gmail.com>
2020-11-05 23:20:32 +0400

    added breakpoint in tablet mode

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 const toggleSwitch = document.querySelector('input[type=checkbox]')
 
+const switchName = document.querySelector('.switchName')
+
 
 function switchTheme(e) {
     if (e.target.checked) {
+        switchName.textContent = "Light Mode"
         document.documentElement.setAttribute('data-theme', "light")
         localStorage.setItem('theme', 'light');
+    
     } else {
         document.documentElement.setAttribute("data-theme", "dark");
         localStorage.setItem('theme', 'dark');
+        switchName.textContent = "Dark Mode"
     }
 }
 
@@ -17,8 +22,10 @@ toggleSwitch.addEventListener('change', switchTheme);
 const currentTheme = localStorage.getItem('theme');
 if(currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme)
+    switchName.textContent = "Dark Mode"
 
     if (currentTheme === "light") {
         toggleSwitch.checked = true;
+        switchName.textContent = "Light Mode"
       }
 }
